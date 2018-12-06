b4dfb27ade180dff05f33c62ded713a2e0ad5dcc
changm.shan <shancm@htgd.com.cn>
2018-12-06 19:33:53 +0800

    1，提交两数之和算法题目

--- LeetCode/src/com/heng/leetcode/MainActivity.java
+++ LeetCode/src/com/heng/leetcode/MainActivity.java
@@ -5,10 +5,11 @@ import com.heng.leetcode.algorithms.LeetAlogorithms;
 public class MainActivity {
     public static void main(String[] args) {
         int[] array = new int[]{1, 3, 5, 7, 9};
-        array = LeetAlogorithms.twoSum(array, 12);
-        StringBuffer result =new StringBuffer();
-        for (int i:array){
-            result.append(" "+i);
+        //array = LeetAlogorithms.twoSum(array, 12);
+        array = LeetAlogorithms.twoSumTwo(array, 12);
+        StringBuffer result = new StringBuffer();
+        for (int i : array) {
+            result.append(" " + i);
         }
         System.out.println(result);
     }
--- LeetCode/src/com/heng/leetcode/algorithms/LeetAlogorithms.java
+++ LeetCode/src/com/heng/leetcode/algorithms/LeetAlogorithms.java
@@ -1,5 +1,8 @@
 package com.heng.leetcode.algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetAlogorithms {
 
     public static int[] twoSum(int[] nums, int target) {
@@ -16,5 +19,18 @@ public class LeetAlogorithms {
         // return null;
         throw new IllegalArgumentException("No two sum solution can reach target");
 
+    }
+
+    public static int[] twoSumTwo(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsValue(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution can reach target");
     }
 }
