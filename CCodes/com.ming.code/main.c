a456b32e2a4a4da065a4eefdba1f964154659cd9
changm.shan <shancm@htgd.com.cn>
2018-12-13 08:02:59 +0800

    1，提交数组和枚举相关示例代码。

--- CCodes/com.ming.code/main.c
+++ CCodes/com.ming.code/main.c
@@ -6,35 +6,65 @@ static int count = 10;
 int conbas;
 
 extern void writen_extern();
-extern void  goto_method();
 
-extern void sum_num(void);
+extern void goto_method();
 
-extern void operator(void);
+extern void sum_num();
 
-extern void extern_method(void);
+extern void operator();
 
-extern void test_print(void);
+extern void extern_method();
 
-extern void before_next(void);
+extern void test_print();
 
-extern void relational_operator(void);
+extern void before_next();
 
-extern void logical_operator(void);
+extern void relational_operator();
 
-extern void bitwise_operation(void);
+extern void logical_operator();
 
-extern void assignment_operation(void);
+extern void bitwise_operation();
 
-extern void miscellaneous_operations(void);
+extern void assignment_operation();
 
-extern void precedence_of_operator(void);
+extern void miscellaneous_operations();
+
+extern void precedence_of_operator();
+
+extern int max_num(int, int);
+
+void switch_num(int, int);
+
+void print_switch();
+
+void test_variable();
+
+extern void array_ges();
+
+extern void twodim_array();
+
+extern double get_average(int arr[], int size);
+
+extern int *getRandom();
+
+void get_random_tesla();
+
+extern void pointer_array();
+
+void test_get_average();
+
+enum {
+    Q, W, E = 4, R
+};
 
 int main() {
     test_print();
+    test_variable();//全局变量与局部变量
+    printf("count is %d\n", count);//查看全局变量打印
     while (count--) {
         sum_num();
     }
+
     extern_method();
     operator();
     before_next();
@@ -45,6 +75,15 @@ int main() {
     miscellaneous_operations();
     precedence_of_operator();
     goto_method();
+    int result = max_num(5, 37);
+    printf("result = max num is %d\n", result);
+    print_switch();
+    array_ges();
+    twodim_array();
+    test_get_average();
+    get_random_tesla();
+    pointer_array();
+    printf("enum Q W E R is : %d , %d , %d , %d", Q, W, E, R);
     return 0;
 }
 
@@ -52,5 +91,43 @@ void sum_num(void) {
     static int another_count = 5;
     another_count++;
     printf(" another_count is %d , count is %d\n", another_count, count);
+
+}
+
+void print_switch() {
+    int a = 100, b = 200;
+    printf("before,a is : %d b is : %d\n", a, b);
+    /* 调用函数来交换值 */
+    switch_num(a, b);
+    printf("after,a is : %d b is : %d\n", a, b);
+}
+
+void test_variable() {
+    printf(" count is %d\n", count);
+    int count = 55;
+    printf(" count is %d\n", count);
+}
+
+void test_get_average() {
+    /* 带有 5 个元素的整型数组 */
+    int balance[5] = {1000, 2, 3, 17, 50};
+    double avg;
+
+    /* 传递一个指向数组的指针作为参数 */
+    avg = get_average(balance, 5);
+
+    /* 输出返回值 */
+    printf("average is:   %f ", avg);
+}
+
+void get_random_tesla() {
+    /* 一个指向整数的指针 */
+    int *p;
+    int i;
+
+    p = getRandom();
+    for (i = 0; i < 10; i++) {
+        printf("*(p + %d) : %d\n", i, *(p + i));
+    }
 
 }
--- CCodes/com.ming.code/support.c
+++ CCodes/com.ming.code/support.c
@@ -13,17 +13,17 @@ void writen_extern(void) {
     printf("conbas is %d\n", conbas);
 }
 
-void goto_method(void){
-    int a=10;
+void goto_method(void) {
+    int a = 10;
     LOOP:
-    do{
-        if (a==15){
-            a=a+1;
+    do {
+        if (a == 15) {
+            a = a + 1;
             goto LOOP;
         }
         a++;
-        printf(  "a is : %d\n", a);
-    }while (a<25);
+        printf("a is : %d\n", a);
+    } while (a < 25);
 }
 
 
@@ -282,4 +282,99 @@ void precedence_of_operator(void) {
 
     e = a + (b * c) / d;     //  20 + (150/5)
     printf("a + (b * c) / d 的值是 %d\n", e);
+}
+
+int max_num(int num, int numT) {
+    return num > numT ? num : numT;
+}
+
+void switch_num(int a, int b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void array_ges(void) {
+    int n[10]; /* n 是一个包含 10 个整数的数组 */
+    int i, j;
+
+    /* 初始化数组元素 */
+    for (i = 0; i < 10; i++) {
+        n[i] = i + 10; /* 设置元素 i 为 i + 100 */
+    }
+
+    /* 输出数组中每个元素的值 */
+    for (j = 0; j < 10; j++) {
+        printf("Element[%d] = %d\n", j, n[j]);
+    }
+}
+
+void twodim_array(void) {
+    /* 一个带有 5 行 2 列的数组 */
+    int a[5][2] = {{0, 1},
+                   {2, 3},
+                   {4, 5},
+                   {6, 7},
+                   {8, 9}};
+    int i, j;
+
+    /* 输出数组中每个元素的值 */
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("a[%d][%d] = %d\n", i, j, a[i][j]);
+        }
+    }
+
+}
+
+double get_average(int arr[], int size) {
+    int i;
+    double avg;
+    double sum;
+
+    for (i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+
+    avg = sum / size;
+
+    return avg;
+}
+
+int *getRandom(void) {
+    static int r[10];
+    int i;
+
+    /* 设置种子 */
+    srand((unsigned) time(NULL));
+    for (i = 0; i < 10; ++i) {
+        r[i] = rand();
+        printf("r[%d] = %d\n", i, r[i]);
+
+    }
+
+    return r;
+}
+
+void pointer_array(){
+    /* 带有 5 个元素的整型数组 */
+    double balance[5] = {1000.0, 2.0, 3.4, 17.0, 50.0};
+    double *p;
+    int i;
+
+    p = balance;
+
+    /* 输出数组中每个元素的值 */
+    printf( "use pointer \n");
+    for ( i = 0; i < 5; i++ )
+    {
+        printf("*(p + %d) : %f\n",  i, *(p + i) );
+    }
+
+    printf( "use balance as pointer \n");
+    for ( i = 0; i < 5; i++ )
+    {
+        printf("*(balance + %d) : %f\n",  i, *(balance + i) );
+    }
 }
