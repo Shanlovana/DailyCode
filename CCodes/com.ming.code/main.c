9c55e2edff3000f4afa94f44c31ef16ae10942db
shanlovana <zhcswlp0625>
2018-12-26 14:51:06 +0800

    1，添加使用libcurl进行网络请求示例。2，添加修改CMakelists相应规则，解决clion运行下对‘libcurl’未定义的引用问题。

--- CCodes/com.ming.code/main.c
+++ CCodes/com.ming.code/main.c
@@ -111,7 +111,10 @@ extern int changeable_varity(char *msg, ...);
 extern void dynamic_allocation_memory();
 
 extern void bubble_sort();
+
 extern void selection_sort();
+extern int getRequest();
+
 #define  message_for(a, b)  \
     printf(#a " and " #b ": We love you!\n")
 
@@ -128,65 +131,68 @@ enum {
 int token998 = 98;
 
 int main() {
-    /*test_print();
-    test_variable();//全局变量与局部变量
-    printf("count is %d\n", count);//查看全局变量打印
-    while (count--) {
-        sum_num();
-    }
-
-    extern_method();
-    operator();
-    before_next();
-    relational_operator();
-    logical_operator();
-    bitwise_operation();
-    assignment_operation();
-    miscellaneous_operations();
-    precedence_of_operator();
-    goto_method();
-    int result = max_num(5, 37);
-    printf("result = max num is %d\n", result);
-    print_switch();
-    array_ges();
-    twodim_array();
-    test_get_average();
-    get_random_tesla();
-    pointer_array();
-    printf("enum Q W E R is : %d , %d , %d , %d", Q, W, E, R);*/
-    // test_function_pointer();
-    //test_assignment_array();
-    //string_test();
-    // book_init();
-    //book_set();
-    //test_bit_field();
-    //struct_array();
-    //union_size();
-    //get_union_mem();
-    //print_mem_size();
-    //print_float();
-    //print_all_you_put();
-    //input_notice();
-    //file_input();
-    //file_output();
-    //macros_predefined();
-    // message_for(ErGouZi, DaMao);
-    //tokenpaste(998);
-    // printf("Here is the message: %s\n", MESSAGE);
-    //hello();
-    //convert_data();
-    //error_deal();
-    /* double a = factorial(10);
-     int b = fabonaci(10);
-     printf(" factorial 10 is %f , fabonaci 10 is %d \n", a, b);
-     double c = average_nums(5, 1, 600, 5, 7, 9);
-     printf(" average_nums 5 is %f ", c);
-     changeable_varity("FAKER", "what", "are", "you", "nong", "sha", "lei", "/0");
-     dynamic_allocation_memory();*/
-    //bubble_sort();
-    selection_sort();
+    getRequest();
     return 0;
 }
+/*test_print();
+test_variable();//全局变量与局部变量
+printf("count is %d\n", count);//查看全局变量打印
+while (count--) {
+    sum_num();
+}
+
+extern_method();
+operator();
+before_next();
+relational_operator();
+logical_operator();
+bitwise_operation();
+assignment_operation();
+miscellaneous_operations();
+precedence_of_operator();
+goto_method();
+int result = max_num(5, 37);
+printf("result = max num is %d\n", result);
+print_switch();
+array_ges();
+twodim_array();
+test_get_average();
+get_random_tesla();
+pointer_array();
+printf("enum Q W E R is : %d , %d , %d , %d", Q, W, E, R);*/
+// test_function_pointer();
+//test_assignment_array();
+//string_test();
+// book_init();
+//book_set();
+//test_bit_field();
+//struct_array();
+//union_size();
+//get_union_mem();
+//print_mem_size();
+//print_float();
+//print_all_you_put();
+//input_notice();
+//file_input();
+//file_output();
+//macros_predefined();
+// message_for(ErGouZi, DaMao);
+//tokenpaste(998);
+// printf("Here is the message: %s\n", MESSAGE);
+//hello();
+//convert_data();
+//error_deal();
+/* double a = factorial(10);
+ int b = fabonaci(10);
+ printf(" factorial 10 is %f , fabonaci 10 is %d \n", a, b);
+ double c = average_nums(5, 1, 600, 5, 7, 9);
+ printf(" average_nums 5 is %f ", c);
+ changeable_varity("FAKER", "what", "are", "you", "nong", "sha", "lei", "/0");
+ dynamic_allocation_memory();*/
+//bubble_sort();
+//selection_sort();
+//return 0;
+
 
 void sum_num(void) {
     static int another_count = 5;
