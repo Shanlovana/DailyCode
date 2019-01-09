26ac50c7fb2c0715aba299b8e2394adbc90c85dc
shanlovana <shanlovana@hotmail.com>
2019-01-09 13:23:29 +0800

    1,将引入的方法添加至method.h文件中

--- CCodes/com.ming.code/main.c
+++ CCodes/com.ming.code/main.c
@@ -6,113 +6,14 @@
 #define HEADER_FILE
 
 #include "operatorhead.h"
+#include "method.h"
 
 #endif
 
 static int count = 10;
 int conbas;
-
-extern void writen_extern();
-
-extern void goto_method();
-
-extern void sum_num();
-
-extern void operator();
-
-extern void extern_method();
-
-extern void test_print();
-
-extern void before_next();
-
-extern void relational_operator();
-
-extern void logical_operator();
-
-extern void bitwise_operation();
-
-extern void assignment_operation();
-
-extern void miscellaneous_operations();
-
-extern void precedence_of_operator();
-
-extern int max_num(int, int);
-
-void switch_num(int, int);
-
-void print_switch();
-
-void test_variable();
-
-extern void array_ges();
-
-extern void twodim_array();
-
-extern double get_average(int arr[], int size);
-
-extern int *getRandom();
-
-void get_random_tesla();
-
-extern void pointer_array();
-
-void test_get_average();
-
-void test_function_pointer();
-
 extern void assignment_array(int *array, size_t arraySize, int (*getrandom_value)(void));
 
-extern int get_random_value();
-
-void test_assignment_array();
-
-extern void string_test();
-
-extern void book_init();
-
-extern void book_set();
-
-extern void test_bit_field();
-
-void union_size();
-
-void get_union_mem();
-
-extern void print_mem_size();
-
-extern void print_float();
-
-extern void struct_array();
-
-extern void print_all_you_put();
-
-extern void input_notice();
-
-extern void file_input();
-
-extern void file_output();
-
-extern void macros_predefined();
-
-extern void convert_data();
-
-extern void error_deal();
-
-extern double factorial(unsigned int);
-
-extern int fabonaci(int);
-
-extern double average_nums(int num, ...);
-
-extern int changeable_varity(char *msg, ...);
-
-extern void dynamic_allocation_memory();
-
-extern void bubble_sort();
-
-extern void selection_sort();
 
 extern int getRequest();
 
