bb9d801b7d4914b6095de9f316e19a3936d15b33
shanlovana <shanlovana@hotmail.com>
2019-01-21 10:46:50 +0800

    1,新建CMake示例项目。提交多个目录，多个源文件CMake处理方法代码。

---
+++ CMakeDemo/math/MathUtils.c
@@ -0,0 +1,5 @@
+//
+// Created by shanlovana on 19-1-21.
+//
+
+#include "MathUtils.h"
---
+++ CMakeDemo/math/MathUtils.h
@@ -0,0 +1,8 @@
+//
+// Created by shanlovana on 19-1-21.
+//
+
+#ifndef CMAKEDEMO_MATHUTILS_H
+#define CMAKEDEMO_MATHUTILS_H
+
+#endif //CMAKEDEMO_MATHUTILS_H
