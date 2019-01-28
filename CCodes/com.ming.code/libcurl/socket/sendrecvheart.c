1c0c27bdbc99bfe6f5ea380de5d11691c2294618
shanlovana <shanlovana@hotmail.com>
2019-01-28 09:40:36 +0800

    1,提交C语言，建立长连接示例，并可循环发送心跳包。

--- CCodes/com.ming.code/libcurl/socket/sendrecvheart.c
+++ CCodes/com.ming.code/libcurl/socket/sendrecvheart.c
@@ -6,6 +6,10 @@
 #include <string.h>
 #include <curl/curl.h>
 
+enum Type {
+    HEART = 0, MSG = 1, EXIT = 2, OTHER = 3
+};
+
 /* Auxiliary function that waits on the socket. */
 static int wait_on_socket(curl_socket_t sockfd, int for_recv, long timeout_ms) {
     struct timeval tv;
@@ -36,7 +40,8 @@ int recieve_send(void) {
     CURL *curl;
     CURLcode res;
     /* Minimalistic http request */
-    const char *request = "GET / HTTP/1.0Host: example.com\r\n\r\n";
+    const char *request = "I`m groot!\r\n";
+
     size_t request_len = strlen(request);
     curl_socket_t sockfd;
     size_t nsent_total = 0;
@@ -80,44 +85,41 @@ int recieve_send(void) {
         }
 
         printf("Sending request.\n");
-
-        do {
-            /* Warning: This example program may loop indefinitely.
-             * A production-quality program must define a timeout and exit this loop
-             * as soon as the timeout has expired. */
-            size_t nsent;
+        //建立通道成功后，将循环发送心跳包，但是区分工作状态,空闲状态
+        for (;;) {
             do {
-                nsent = 0;
-                res = curl_easy_send(curl, request + nsent_total,
-                                     request_len - nsent_total, &nsent);
-                nsent_total += nsent;
+                size_t nsent;
+                nsent_total = 0;
+                do {
+                    nsent = 0;
+                    res = curl_easy_send(curl, request + nsent_total,
+                                         request_len - nsent_total, &nsent);
+                    nsent_total += nsent;
 
-                if (res == CURLE_AGAIN && !wait_on_socket(sockfd, 0,  60 * 1000L)) {
-                    printf("Error: timeout.\n");
+                    if (res == CURLE_AGAIN && !wait_on_socket(sockfd, 0, 30 * 1000L)) {
+                        printf("Error: timeout.\n");
+                        return 1;
+                    }
+                } while (res == CURLE_AGAIN);
+
+                if (res != CURLE_OK) {
+                    printf("Error: %s\n", curl_easy_strerror(res));
                     return 1;
                 }
-            } while (res == CURLE_AGAIN);
+                printf("Sent %" CURL_FORMAT_CURL_OFF_T " bytes.\n",
+                       (curl_off_t) nsent);
 
-            if (res != CURLE_OK) {
-                printf("Error: %s\n", curl_easy_strerror(res));
-                return 1;
-            }
-            printf("Sent %" CURL_FORMAT_CURL_OFF_T " bytes.\n",
-                   (curl_off_t) nsent);
+            } while (nsent_total < request_len);
 
-        } while (nsent_total < request_len);
+            printf("Reading response.\n");
 
-        printf("Reading response.\n");
-
-        for (;;) {
-            /* Warning: This example program may loop indefinitely (see above). */
             char buf[1024];
             size_t nread;
             do {
                 nread = 0;
                 res = curl_easy_recv(curl, buf, sizeof(buf), &nread);
 
-                if (res == CURLE_AGAIN && !wait_on_socket(sockfd, 1, 60 * 1000L)) {
+                if (res == CURLE_AGAIN && !wait_on_socket(sockfd, 1, 30 * 1000L)) {
                     printf("Error: timeout.\n");
                     return 1;
                 }
@@ -136,9 +138,11 @@ int recieve_send(void) {
                    buf);
             printf("Received %" CURL_FORMAT_CURL_OFF_T " bytes.\n",
                    (curl_off_t) nread);
-        }
 
+            sleep(3);
+        }
         /* always cleanup */
+        printf("Received curl_easy_cleanup is curl_easy_cleanup ");
         curl_easy_cleanup(curl);
     }
     return 0;
