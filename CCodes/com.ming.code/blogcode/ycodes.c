f4b0136d87b4c22915d069cd36796babcee1ac07
changm.shan <htgd@1234>
2019-03-12 13:21:54 +0800

    1,提交C语言补充学习，百度广告点击器

---
+++ CCodes/com.ming.code/blogcode/ycodes.c
@@ -0,0 +1,14 @@
+//
+// Created by android on 19-3-11.
+//
+
+#include "ycodes.h"
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void  open(char *str){
+
+}
---
+++ CCodes/com.ming.code/blogcode/ycodes.h
@@ -0,0 +1,8 @@
+//
+// Created by android on 19-3-11.
+//
+
+#ifndef CCODES_YCODES_H
+#define CCODES_YCODES_H
+
+#endif //CCODES_YCODES_H
